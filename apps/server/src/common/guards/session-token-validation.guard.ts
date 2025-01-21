9f075faad92a913d12c73e44553f943905d359e5
wlgh1553 <jiho30125@gmail.com>
2025-01-21 18:42:50 +0900

    feat: improve guard performance using caching strategy with redis
    Co-authored-by: shl0501 <shl0501@users.noreply.github.com>
    Co-authored-by: 유영재 <yu-yj215@users.noreply.github.com>

--- apps/server/src/common/guards/session-token-validation.guard.ts
+++ apps/server/src/common/guards/session-token-validation.guard.ts
@@ -1,13 +1,19 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
 
 import { SessionsRepository } from '@sessions/sessions.repository';
 import { SessionsAuthRepository } from '@sessions-auth/sessions-auth.repository';
 
 @Injectable()
 export class SessionTokenValidationGuard implements CanActivate {
+  private readonly ttl = 300; // 5분
+
   constructor(
     private readonly sessionsRepository: SessionsRepository,
     private readonly sessionsAuthRepository: SessionsAuthRepository,
+
+    @Inject('REDIS_SESSION') private readonly sessionRedisClient: Redis,
+    @Inject('REDIS_TOKEN') private readonly tokenRedisClient: Redis,
   ) {}
 
   async validateSessionToken(sessionId: string, token: string) {
@@ -15,18 +21,37 @@ export class SessionTokenValidationGuard implements CanActivate {
       throw new ForbiddenException('세션 ID와 사용자 토큰이 필요합니다.');
     }
 
-    const session = await this.sessionsRepository.findById(sessionId);
-    if (!session) {
-      throw new ForbiddenException('세션이 존재하지 않습니다.');
-    }
-
+    const sessionKey = sessionId;
+    const expiredDate = await this.sessionRedisClient.get(sessionKey);
     const currentTime = new Date();
-    if (session.expiredAt && session.expiredAt < currentTime) {
-      throw new ForbiddenException('세션이 만료되었습니다.');
+
+    if (!expiredDate) {
+      const session = await this.sessionsRepository.findById(sessionId);
+      if (!session) {
+        throw new ForbiddenException('세션이 존재하지 않습니다.');
+      }
+      if (session.expiredAt && session.expiredAt < currentTime) {
+        throw new ForbiddenException('세션이 만료되었습니다.');
+      }
+      await this.sessionRedisClient.set(sessionKey, session.expiredAt.toISOString(), 'EX', this.ttl);
+    } else {
+      const expiredDateTime = new Date(expiredDate);
+      if (expiredDateTime < currentTime) {
+        await this.sessionRedisClient.del(sessionKey);
+        throw new ForbiddenException('세션이 만료되었습니다.');
+      }
     }
 
-    const userSessionToken = await this.sessionsAuthRepository.findByToken(token);
-    if (!userSessionToken || userSessionToken.sessionId !== sessionId) {
+    const tokenKey = token;
+    const sessionValue = await this.tokenRedisClient.get(tokenKey);
+    if (!sessionValue) {
+      const userSessionToken = await this.sessionsAuthRepository.findByToken(token);
+      if (!userSessionToken || userSessionToken.sessionId !== sessionId) {
+        throw new ForbiddenException('해당 세션에 접근할 권한이 없습니다.');
+      }
+
+      await this.tokenRedisClient.set(tokenKey, userSessionToken.sessionId, 'EX', this.ttl);
+    } else if (sessionValue !== sessionId) {
       throw new ForbiddenException('해당 세션에 접근할 권한이 없습니다.');
     }
   }
---
+++ apps/server/src/common/redis.module.ts
@@ -0,0 +1,30 @@
+import { Global, Module } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'REDIS_SESSION',
+      useFactory: () => {
+        return new Redis({
+          host: process.env.REDIS_HOST,
+          port: Number(process.env.REDIS_PORT),
+          db: 0, // 세션용 DB
+        });
+      },
+    },
+    {
+      provide: 'REDIS_TOKEN',
+      useFactory: () => {
+        return new Redis({
+          host: process.env.REDIS_HOST,
+          port: Number(process.env.REDIS_PORT),
+          db: 1, // 토큰용 DB
+        });
+      },
+    },
+  ],
+  exports: ['REDIS_SESSION', 'REDIS_TOKEN'],
+})
+export class RedisModule {}
