44fd614ca40256d3cece6189808f70eae8127bb7
Choi Jeongmin <cjeongmin27@gmail.com>
2025-01-21 14:11:35 +0900

    fix(fe): refactor authentication checks to use accessToken (#32)
    * refactor: remove isLogin method from auth store
    
    * fix: replace isLogin method with accessToken checks in auth store

--- apps/client/src/features/auth/auth.store.ts
+++ apps/client/src/features/auth/auth.store.ts
@@ -3,13 +3,11 @@ import { create } from 'zustand';
 interface AuthStore {
   userId?: number;
   accessToken?: string;
-  isLogin: () => boolean;
   setAuthInformation: ({ userId, accessToken }: { userId?: number; accessToken?: string }) => void;
   clearAuthInformation: () => void;
 }
 
-export const useAuthStore = create<AuthStore>((set, get) => ({
-  isLogin: () => get().accessToken != null,
+export const useAuthStore = create<AuthStore>((set) => ({
   setAuthInformation: ({ userId, accessToken }: { userId?: number; accessToken?: string }) =>
     set({ userId, accessToken }),
   clearAuthInformation: () => set({ userId: undefined, accessToken: undefined }),
