1e18424ef330e151b68fef448566e7c9a5c080e9
cjeongmin <cjeongmin27@gmail.com>
2025-02-04 21:36:37 +0900

    feat: add API for question improvement

---
+++ apps/client/src/features/support-writing-question/api/improve-question.api.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { z } from 'zod';
+
+export const QuestionImprovementRequestSchema = z.object({
+  token: z.string(),
+  sessionId: z.string(),
+  body: z.string(),
+});
+
+export const QuestionImprovementResponseSchema = z.object({
+  result: z.object({
+    question: z.string(),
+  }),
+});
+
+export type QuestionImprovementRequest = z.infer<typeof QuestionImprovementRequestSchema>;
+
+export type QuestionImprovementResponse = z.infer<typeof QuestionImprovementResponseSchema>;
+
+export const postQuestionImprovement = (body: QuestionImprovementRequest) =>
+  axios
+    .post<QuestionImprovementResponse>('/api/ai/question-improve', QuestionImprovementRequestSchema.parse(body))
+    .then((res) => QuestionImprovementResponseSchema.parse(res.data));
