fa76ec42579fe4d3273947a0e2ba0f2efa38be3f
cjeongmin <cjeongmin27@gmail.com>
2025-02-04 16:08:31 +0900

    fix: change button to div for modal background and add role attribute

--- apps/client/src/shared/ui/modal/useModal.tsx
+++ apps/client/src/shared/ui/modal/useModal.tsx
@@ -33,7 +33,8 @@ function Background({ children }: Readonly<PropsWithChildren>) {
   }, [closeModal]);
 
   return (
-    <button
+    <div
+      role='dialog'
       onClick={(e) => {
         e.stopPropagation();
         if (e.target === e.currentTarget) closeModal();
@@ -44,7 +45,7 @@ function Background({ children }: Readonly<PropsWithChildren>) {
       className='fixed left-0 top-0 z-10 flex h-dvh w-dvw cursor-auto items-center justify-center bg-[#808080]/20 backdrop-blur-sm'
     >
       {children}
-    </button>
+    </div>
   );
 }
 
