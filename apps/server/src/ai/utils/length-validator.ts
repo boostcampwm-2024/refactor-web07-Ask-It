198d25d670fd22fdec3e2009e60e84251141ec12
yu-yj215 <yuyoungjae@naver.com>
2025-02-12 19:58:58 +0900

    refactor: convert function to class method

--- apps/server/src/ai/utils/length-validator.ts
+++ apps/server/src/ai/utils/length-validator.ts
@@ -2,10 +2,24 @@ import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface 
 
 @ValidatorConstraint({ name: 'ContentLengthValidator', async: false })
 export class ContentLengthValidator implements ValidatorConstraintInterface {
+  getContentBodyLength = (body: string) => {
+    const regex = /!?\[(\w+)\]\([^)]+\)/g;
+    const matches = (body.match(regex) || []) as RegExpMatchArray;
+
+    return matches.reduce((length, match) => {
+      const textContent = /\[(\w+)\]/.exec(match)?.[1] || '';
+      return length - match.trim().length + textContent.trim().length;
+    }, body.trim().length);
+  };
+
+  isValidBodyLength = (body: number, restrictNumber: number) => {
+    return body > 0 && body <= restrictNumber;
+  };
+
   validate(body: string, args: ValidationArguments) {
     const maxLength = args.constraints[0] || 500;
-    const length = getContentBodyLength(body);
-    return isValidBodyLength(length, maxLength);
+    const length = this.getContentBodyLength(body);
+    return this.isValidBodyLength(length, maxLength);
   }
 
   defaultMessage(args: ValidationArguments) {
@@ -13,17 +27,3 @@ export class ContentLengthValidator implements ValidatorConstraintInterface {
     return `본문 글자 수는 1~${maxLength}자 사이여야 합니다.`;
   }
 }
-
-const getContentBodyLength = (body: string) => {
-  const regex = /!?\[(\w+)\]\([^)]+\)/g;
-  const matches = (body.match(regex) || []) as RegExpMatchArray;
-
-  return matches.reduce((length, match) => {
-    const textContent = /\[(\w+)\]/.exec(match)?.[1] || '';
-    return length - match.trim().length + textContent.trim().length;
-  }, body.trim().length);
-};
-
-const isValidBodyLength = (body: number, restrictNumber: number) => {
-  return body > 0 && body <= restrictNumber;
-};
