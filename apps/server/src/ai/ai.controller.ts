92ec60b86dc69ddb507bb8bb75819db60de0dfe5
yu-yj215 <yuyoungjae@naver.com>
2025-02-12 17:47:26 +0900

    feat: add question retry API

--- apps/server/src/ai/ai.controller.ts
+++ apps/server/src/ai/ai.controller.ts
@@ -4,12 +4,12 @@ import { ApiBody } from '@nestjs/swagger';
 import { AiService } from './ai.service';
 import { CreateHistoryDto } from './dto/create-history.dto';
 import { ImproveQuestionDto } from './dto/improve-question.dto';
-import { ShortenQuestionDto } from './dto/shorten-question.dto';
 import { AiRequestValidationGuard } from './guards/restrict-request.guard';
 import { CreateHistorySwagger } from './swagger/create-history.swagger';
 import { ImproveQuestionSwagger } from './swagger/improve-question.swagger';
-import { ShortenQuestionSwagger } from './swagger/shorten-question.swagger';
 
+import { RetryImproveDto } from '@ai/dto/retry-question.dto';
+import { RetryQuestionSwagger } from '@ai/swagger/retry-question.swagger';
 import { SessionTokenValidationGuard } from '@common/guards/session-token-validation.guard';
 
 @Controller('ai')
@@ -27,20 +27,18 @@ export class AiController {
     return { result };
   }
 
-  @Post('question-shorten')
-  @ShortenQuestionSwagger()
-  @ApiBody({ type: ShortenQuestionDto })
-  @UseGuards(SessionTokenValidationGuard)
-  public async shortenQuestion(@Body() shortenQuestionDto: ShortenQuestionDto) {
-    const { body: userContent } = shortenQuestionDto;
-    const result = { question: await this.aiService.requestShortenQuestion(userContent) };
-    return { result };
-  }
-
   @Post('history')
   @CreateHistorySwagger()
   @ApiBody({ type: CreateHistoryDto })
   public createHistory(@Body() createHistoryDto: CreateHistoryDto) {
     this.aiService.createHistory(createHistoryDto);
+  }
+
+  @Post('question-improve-retry')
+  @RetryQuestionSwagger()
+  @UseGuards(SessionTokenValidationGuard)
+  public async retryImproveQuestion(@Body() retryImproveDto: RetryImproveDto) {
+    const result = { question: await this.aiService.retryImproveQuestion(retryImproveDto) };
+    return { result };
   }
 }
---
+++ apps/server/src/ai/dto/retry-question.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+import { BaseDto } from '@common/base.dto';
+
+export class RetryImproveDto extends BaseDto {
+  @ApiProperty({
+    example: '호날두 vs 메시',
+    description: '원본 질문 내용',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty({ message: '원본 질문은 필수입니다.' })
+  original: string;
+
+  @ApiProperty({
+    example: '## 축구선수 질문입니다. ### 호날두와 메시 중 더 뛰어난 축구선수가 누굽니까?',
+    description: '개선할 질문 내용',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty({ message: '개선할 질문 내용은 필수입니다.' })
+  received: string;
+
+  @ApiProperty({
+    example: '말투를 더 완곡하게 변경해주세요.',
+    description: '질문 개선 시 추가 요청할 내용 (비어있다면 빈 문자열로 보내주세요.)',
+    required: true,
+  })
+  @IsString()
+  retryMessage: string;
+}
