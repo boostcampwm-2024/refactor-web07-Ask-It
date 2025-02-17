9456cb7def6dc6055b5f22030790adbfa814acd9
Choi Jeongmin <cjeongmin27@gmail.com>
2025-02-17 17:51:04 +0900

    feat(be): add slang detection (#94)
    * feat: add slang prediction endpoint and update model structure
    
    * feat: update prediction functions to return probabilities and add slang prediction response model

--- apps/classifier/model.py
+++ apps/classifier/model.py
@@ -2,32 +2,43 @@ import torch
 from transformers import BertTokenizerFast, BertForSequenceClassification
 
 tokenizer = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
-reply_classifier_model = BertForSequenceClassification.from_pretrained(
-    "./reply-classifier-model"
-)
-reply_classifier_model.eval()
-question_classifier_model = BertForSequenceClassification.from_pretrained(
-    "./question-classifier-model"
-)
 
-reply_inv_label_map = {0: "나쁨", 1: "좋음"}
-question_inv_label_map = {
-    5: "매우 좋음",
-    4: "좋음",
-    3: "미묘한 개선",
-    2: "변경 없음",
-    1: "과도한 변경",
-    0: "매우 나쁨",
+models = {
+    "reply": BertForSequenceClassification.from_pretrained("./reply-classifier-model"),
+    "question": BertForSequenceClassification.from_pretrained(
+        "./question-classifier-model"
+    ),
+    "slang": BertForSequenceClassification.from_pretrained("./slang-classifier-model"),
+}
+
+models["reply"].eval()
+models["question"].eval()
+models["slang"].eval()
+
+inv_label_maps = {
+    "reply": {0: "나쁨", 1: "좋음"},
+    "question": {
+        5: "매우 좋음",
+        4: "좋음",
+        3: "미묘한 개선",
+        2: "변경 없음",
+        1: "과도한 변경",
+        0: "매우 나쁨",
+    },
+    "slang": {
+        0: "일반어",
+        1: "욕설",
+    },
 }
 
 
-def predict(text: str, type: str) -> str:
+def predict(text: str, type: str) -> tuple[str, float]:
     inputs = tokenizer(
         text, return_tensors="pt", truncation=True, padding="max_length", max_length=512
     )
 
-    model = reply_classifier_model if type == "reply" else question_classifier_model
-    inv_label_map = reply_inv_label_map if type == "reply" else question_inv_label_map
+    model = models[type]
+    inv_label_map = inv_label_maps[type]
 
     with torch.no_grad():
         outputs = model(**inputs)
@@ -35,5 +46,6 @@ def predict(text: str, type: str) -> str:
     logits = outputs.logits
     probabilities = torch.softmax(logits, dim=-1)
     predicted_label = torch.argmax(probabilities, dim=-1).item()
+    predicted_probability = probabilities[0, predicted_label].item()
 
-    return inv_label_map[predicted_label]
+    return inv_label_map[predicted_label], predicted_probability
--- apps/classifier/schemas.py
+++ apps/classifier/schemas.py
@@ -1,6 +1,30 @@
 from pydantic import BaseModel
 
 
+class SlangPredictionRequest(BaseModel):
+    input: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "input": "X같네",
+            }
+        }
+
+
+class SlangPredictionResponse(BaseModel):
+    predicted: str
+    probability: float
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "predicted": "욕설",
+                "probability": 0.99,
+            }
+        }
+
+
 class PredictionRequest(BaseModel):
     input: str
     output: str
