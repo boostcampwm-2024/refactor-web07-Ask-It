5efd4b1c57fa64f8ada01fed7aed13f1d65382f9
cjeongmin <cjeongmin27@gmail.com>
2025-02-17 11:14:23 +0900

    refactor: simplify FastAPI initialization in improve reply prediction API

--- apps/classifier/app.py
+++ apps/classifier/app.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 from schemas import PredictionRequest, PredictionResponse
 from model import predict
 
-app = FastAPI(
-    title="Improve Reply Prediction API",
-    description="입력된 질문과 답변에 대해 '좋음' 또는 '나쁨'을 예측하는 API",
-    version="1.0.0",
-)
+app = FastAPI()
 
 
 @app.post("/improve-question-predict", response_model=PredictionResponse)
