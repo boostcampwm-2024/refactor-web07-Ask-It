80370df99fbb60be5810b6945bdc03b03aaf71fb
shl0501 <shl050105010501@gmail.com>
2025-02-18 16:00:56 +0900

    feat: restrict request by using token and ip

--- apps/server/src/ai/guards/restrict-request.guard.ts
+++ apps/server/src/ai/guards/restrict-request.guard.ts
@@ -5,20 +5,32 @@ import Redis from 'ioredis';
 export class AiRequestValidationGuard implements CanActivate {
   private readonly ttl = 3;
 
-  constructor(@Inject('REDIS_RESTRICT_TOKEN') private readonly restrictRedisClient: Redis) {}
-
-  async validateAiRequest(token: string) {
-    const exists = await this.restrictRedisClient.get(token);
-    if (Number(exists) === 1) {
+  constructor(
+    @Inject('REDIS_RESTRICT_TOKEN') private readonly restrictTokenRedis: Redis,
+    @Inject('REDIS_RESTRICT_IP') private readonly restrictIpRedis: Redis,
+  ) {}
+  async validateAiRequest(token: string, ip: string) {
+    const [tokenExists, ipExists] = await Promise.all([
+      this.restrictTokenRedis.get(token),
+      this.restrictIpRedis.get(ip),
+    ]);
+    if (tokenExists || ipExists) {
       throw new ForbiddenException('저희의 토큰은 소중합니다.');
     }
-    await this.restrictRedisClient.set(token, 1, 'EX', this.ttl);
+    await Promise.all([
+      this.restrictTokenRedis.set(token, 'true', 'EX', this.ttl),
+      this.restrictIpRedis.set(ip, 'true', 'EX', this.ttl),
+    ]);
   }
+
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const token = request.body?.token || request.query?.token;
 
-    await this.validateAiRequest(token);
+    const ip =
+      request.headers['x-forwarded-for']?.toString().split(',')[0] || request.socket.remoteAddress || request.ip;
+
+    await this.validateAiRequest(token, ip);
     return true;
   }
 }
