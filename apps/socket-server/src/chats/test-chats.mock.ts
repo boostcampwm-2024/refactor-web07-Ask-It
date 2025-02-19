f33041b06bfef2741d878ee024fa465b2ac6b559
wlgh1553 <jiho30125@gmail.com>
2025-02-19 13:28:22 +0900

    test: update chatting unit test

--- apps/socket-server/src/chats/test-chats.mock.ts
+++ apps/socket-server/src/chats/test-chats.mock.ts
@@ -1,6 +1,5 @@
-import { AbuseState } from '@prisma/client';
-
 import { Roles } from '@common/roles/roles';
+import { AbuseState } from '@prisma/client';
 
 export const MOCK_DATE = new Date('2024-01-01T00:00:00.000Z');
 
