23f70057a792563531f57170b4a20663e5eb2bea
wlgh1553 <jiho30125@gmail.com>
2025-02-19 14:20:19 +0900

    style: update cleanbot alert message

--- apps/client/src/entities/session/ui/ChattingMessage.tsx
+++ apps/client/src/entities/session/ui/ChattingMessage.tsx
@@ -10,7 +10,7 @@ function ChattingMessage({ showAbuseMessage, chat: { nickname, content, abuse } 
     <div className='inline-flex flex-col items-start justify-start gap-1 self-stretch p-2.5'>
       <span className='flex-shrink-0 text-sm font-semibold text-indigo-600'>{nickname}</span>
       <span className='shrink grow basis-0 break-all text-sm font-medium text-black'>
-        {abuse && showAbuseMessage ? '클린봇이 부적절한 표현을 감지한 댓글입니다.' : content}
+        {abuse && showAbuseMessage ? '클린봇이 부적절한 표현을 감지한 채팅입니다.' : content}
       </span>
     </div>
   );
